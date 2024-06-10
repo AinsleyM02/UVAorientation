fbfbce29d6a49cfa577f6f722d9ead751d5a622f
Ainsley McLaughlin <annerositamc01@gmail.com>
2024-06-10 16:20:03 -0400

    commit

--- Orientation.py
+++ Orientation.py
@@ -1,1 +1,19 @@
 #this is a test file
+#using .py signifies its a python script
+#cntrl S to save it
+
+#when you clone a git hub repository you get a copy on your local machine
+#to clone: Connect github repsity to vs code: click on home of repository>
+#green code button>make sure on https> hit the copy on righ side icon>go to vs code
+#>do cntl Shift p (this pulls up all command you could do for vs code)> type in "git clone"
+#>past the url you copied from repositroy> enter>click the folder you want the reposity contained in> then select as repository destination
+#then can use commands to push and pull things to the main repository hosted online
+
+
+#if I wanted to upload this file from vs code to github: open terminal: view>terminal>
+#arrow beside the>gitbash>go to actual terminal line>git add . >
+#git commit -m "commit message"> git push
+
+#edit everything from vs code and then push it up, run all of the commands above again to "save the changes"
+
+
