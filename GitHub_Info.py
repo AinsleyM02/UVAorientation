72616343f8909099f266123ac0127db04d48b247
Ainsley McLaughlin <annerositamc01@gmail.com>
2024-06-11 15:50:32 -0400

    Update GitHub_Info.py

--- GitHub_Info.py
+++ GitHub_Info.py
@@ -3,3 +3,6 @@
 # #that README will automatically appear on your profile page. You can edit your profile README 
 # #with GitHub Flavored Markdown to create a personalized section on your profile. 
 # #For more information, see "Managing your profile README."
+
+#helpful link to format markdown text as in this document:
+#https://docs.github.com/en/get-started/writing-on-github/getting-started-with-writing-and-formatting-on-github/quickstart-for-writing-on-github
