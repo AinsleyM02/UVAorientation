3d6d4576067343630b0d4680c7fa3f1215c09e25
Ainsley McLaughlin <annerositamc01@gmail.com>
2024-06-11 16:51:05 -0400

    commit

---
+++ VS_code_information.py
@@ -0,0 +1,6 @@
+#VS code information
+#Shells, focus on the difference between a Graphical user interface (GUI) and a 
+# Command line interface (CLI)
+#Shell: the program that takes in the commands that you do on a computer via gui or cli and has the computer react
+#GUI: graphical user interface: like the app that a use can click on with a mouse and then the shell interprets and acts on them
+# CLI: Command line interface: If you open the terminal and type the actions and then the shell interprets and acts on them
